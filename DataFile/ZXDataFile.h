3310b56cf3940c96ad2728d2b585fae778a6d018
Du Zixi <duzixi@gmail.com>
2018-10-18 12:34:44 +0800

    Rename ZXDataFile.h to DataFile/ZXDataFile.h

--- DataFile/ZXDataFile.h
+++ DataFile/ZXDataFile.h
@@ -1,17 +1,17 @@
 // ================================================================
 //  ZXDataFile.h
-//  ���ܶ�λ�����������ļ���ʽת��
-//  ���ݵ����ļ���ʽ�����������ݽṹ���жϸ�ʽ���ͣ�����ת��ΪĿ���ʽ
+//  功能定位：点云数据文件格式转换
+//  根据点云文件格式名，点云数据结构，判断格式类型，进而转换为目标格式
 // ----------------------------------------------------------------
-//  Created by ������ on 2018.8.2
+//  Created by 杜子兮 on 2018.8.2
 // ================================================================
 
 #pragma once
 
 enum DataFormat
 {
-	// ���¾�Ϊ�ı���ʽ
-	F_TXT, // x,y,z  �Լ�Լ���ģ������ָ����������������
+	// 以下均为文本格式
+	F_TXT, // x,y,z  自己约定的，如果不指定，则按这个类型输出
 	F_OFF, // x y z
 	F_PCD, // x y z
 	F_PLY, // x y z
@@ -27,12 +27,12 @@ public:
 	ZXDataFile(void);
 	~ZXDataFile(void);
 
-	// ����ת��
-	// ��ȡ����
+	// 数据转换
+	// 提取点云
 	static bool Convert(char *inputFileName, DataFormat format);
 
 private:
-	// �����ļ���չ�����ж��ļ�����
+	// 根据文件扩展名，判断文件类型
 	static DataFormat GetFormatByFileName(char *fileName);
 };
 
